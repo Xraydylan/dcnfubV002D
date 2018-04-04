f4b707bb9c8a6d8c8441973001d08b91bab21bea
Xraydylan <xraydylan@gmail.com>
2018-04-04 23:36:20 +0200

    Edited help

--- commands/cmd_help.py
+++ commands/cmd_help.py
@@ -1,6 +1,6 @@
 import discord
 from os import path
-from use import use
+from use import use,get
 
 async def ex(args, message, client, invoke, server):
     c_titel = "Commands"
@@ -9,13 +9,13 @@ async def ex(args, message, client, invoke, server):
     if path.isfile(f):
         with open(f) as f:
             contentpre = f.read()
-        role_master = discord.utils.get(server.roles, name="RPG-Master")
-        if role_master == None:
+        rmaster = get.member_by_role(server, discord.utils.get(server.roles, name="RPG-Master"))
+        bmaster = get.member_by_role(server, discord.utils.get(server.roles, name="Bot Master"))
+        if rmaster == None or bmaster == None:
             await use.error("Something in the code went wrong!", message.channel, client)
         else:
-            master = next((x for x in server.members if role_master in x.roles))
             try:
-                content = contentpre % master.mention
+                content = contentpre % (rmaster.mention, bmaster.mention)
             except:
                 await use.error("Something went wrong!", message.channel, client)
     await client.send_message(message.author, embed=discord.Embed(color=discord.Color.green(), title=c_titel, description=content))
